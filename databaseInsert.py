494d4c9b6911602a3b01a74d2558195c44ec0a8a
kiana525 <jixujia123@163.com>
2017-04-19 19:53:02 -0400

    Update and rename databaseInsert to databaseInsert.py

--- databaseInsert.py
+++ databaseInsert.py
@@ -3,11 +3,10 @@ import sys
 
 con = lite.connect('finger.db')
 cursor = connection.cursor()
-newname = None
+newname = "Xujia Ji"
 
 with con:
-    
 	cur = con.cursor()    
 	cur.execute("CREATE TABLE FINGER(ID INT, Name TEXT)")
 	cur.execute("INSERT INTO FINGER VALUES(newname)")
-conn.close()
+con.close()
